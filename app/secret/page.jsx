03877d048a6640101c439ea00833a6b1579c8322
Anas <anassaif.507@gmail.com>
2024-05-11 11:44:23 +0530

    initt

---
+++ app/secret/page.jsx
@@ -0,0 +1,36 @@
+'use client'
+ 
+import { useSearchParams } from 'next/navigation'
+// import { LoginSocialUser } from '@/redux/slices/authSlice';
+// import { useEffect } from 'react';
+// import {jwtDecode} from 'jwt-decode';
+// import { useDispatch } from 'react-redux';
+ 
+const Secret = () =>{
+  const searchParams = useSearchParams()
+//   const dispatch = useDispatch();
+
+  // useEffect(() => {
+    const token = searchParams.get('token');
+    console.log("Token", token);
+    // let decodedToken;
+    // if (token) {
+    //   decodedToken = jwtDecode(token);
+    //   console.log("Decoded",decodedToken)
+    //   // dispatch(LoginSocialUser({token,socialUser:decodedToken}))
+    // }
+  // }, [])
+
+//   useEffect(() => {
+//     console.log("Hello,  UseEffect,,,,", token)
+//     dispatch(LoginSocialUser({token, socialUser:decodedToken}))
+//   }, [])
+  
+  
+ 
+  // URL -> `/dashboard?search=my-project`
+  // `search` -> 'my-project'
+  return <>Search: {token}</>
+}
+
+export default Secret;
